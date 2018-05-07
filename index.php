cd024e60c5c160b899d6b7d39da630a7ef1020ec
Estelle <valat.estelle@gmail.com>
2018-05-07 23:18:47 +0200

    suite

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Accueil</title>
+  </head>
+  <body>
+    <!--navbar-->
 
+    <?php // TODO: menu d'onglets ?>
+
+  </body>
+</html>
